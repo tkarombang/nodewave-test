c0048b3a3be7ab485ff1385597a45fa8d70c7ae8
tkarombang <tuangkarombang@gmail.com>
2025-07-19 20:38:31 +0800

    Fix-Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,62 +1,47 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { api } from "@/lib/api";
-import { useAuthStore } from "@/store/useAuthStore";
-import { loginSchema } from "@/validators/authSchema";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import Link from "next/link";
 
 export default function LoginPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<loginSchema>({
-    resolver: zodResolver(loginSchema),
-  });
-
-  const router = useRouter();
-  const setToken = useAuthStore((state) => state.setToken);
-  const [loading, setLoading] = useState(false);
-
-  const onSubmit = async (data: loginSchema) => {
-    setLoading(true);
-    try {
-      const res = await api.post("auth/login", data);
-      const token = res.data?.token;
-      setToken(token);
-      toast.success("LOGIN BERHASIL");
-      router.push("/todos");
-    } catch (err: any) {
-      toast.error("EMAIL ATAU PASSWORD SALAH");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="flex items-center justify-center  min-h-screen bg-gray-100 p-4">
-      <form onSubmit={handleSubmit(onSubmit)} className="bg-white p6 rounded-xl shadow-md w-full max-w-md space-y-4">
-        <h1 className="text-2xl font-bold  text-center">Login</h1>
-
-        <div>
-          <Input {...register("email")} placeholder="Email" />
+    <div className="min-h-screen flex items-center justify-center bg-background">
+      <div className="w-full max-w-md space-y-6 px-6 py-8 bg-white rounded-xl shadow">
+        <div className="text-center">
+          <h1 className="text-center text-3xl font-bold text-stone-600">Sign In</h1>
+          <p className="text-muted-foreground mt-2">Just sign in if you have an account in here. Enjoy our Website 😋</p>
         </div>
 
-        <div>
-          <Input type="password" {...register("password")} placeholder="Password" />
-          {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
-        </div>
+        <div className="space-y-4">
+          <div className="text-stone-600">
+            {/* <h3>Your Email / Username</h3> */}
+            <Input type="email" id="email" placeholder="Enter your email / username" />
+          </div>
 
-        <Button type="submit" className="w-full" disabled={loading}>
-          {loading ? "Logging in..." : "Login"}
-        </Button>
-      </form>
+          <div>
+            <Input type="password" id="password" placeholder="Enter your password"></Input>
+          </div>
+
+          <div className="flex items-center justify-between text-sm">
+            <div className="flex items-center gap-2">
+              <Checkbox id="remember" />
+              <h3>Remember me</h3>
+            </div>
+            <Link href="#" className="text-blue-500 hover:underline">
+              Forgot Password
+            </Link>
+          </div>
+
+          <Button className="w-full">Login Button</Button>
+
+          <p className="text-center text-sm text-muted-foreground mt-4">
+            Don&apos;t have an account?{" "}
+            <Link href="/register" className="text-blue-500 hover:underline">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
